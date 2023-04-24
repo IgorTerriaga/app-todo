bca63f7414e49e7f953056701050318d9ef4b25a
igor.terriaga <igor.terriaga@nexxera.com>
2023-04-24 07:59:46 -0300

    app finish

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnChanges, OnInit } from '@angular/core';
+
+//Model
 import { TaskList } from '../../model/task-list';
 
 @Component({
@@ -6,22 +8,47 @@ import { TaskList } from '../../model/task-list';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent implements DoCheck {
 
-  public taskList: Array<TaskList> = [{ task: "Minha nova Task", checked: true },
-  { task: "Minha nova Task 2", checked: false }];
+  public taskList: Array<TaskList> = JSON.parse(localStorage.getItem("list") || '[]');
   constructor() { }
 
-  ngOnInit(): void {
+  ngDoCheck() {
+    this.setLocalStorage()
   }
-  public deleteItem(event: number) {
-    this.taskList.splice(event, 1);
+
+  public setEmitTaskList(event: string) {
+    return this.taskList.push({ task: event, checked: false });
   }
+
+  public deleteItemTaskList(event: number) {
+    return this.taskList.splice(event, 1);
+  }
+
   public deleteAllTaskList() {
-    const confirm = window.confirm("Você deseja confirmar a exclusão?");
+    const confirm = window.confirm("Tem certeza que deseja Deletar tudo?");
+
     if (confirm) {
       this.taskList = [];
     }
   }
 
+  public validationInput(event: string, index: number) {
+
+    if (!event.length) {
+      const confirm = window.confirm("Task está sem vazia, deseja deletar?");
+
+      if (confirm) {
+        this.deleteItemTaskList(index);
+      }
+    }
+
+  }
+
+  public setLocalStorage() {
+    if (this.taskList) {
+      this.taskList.sort((first, last) => Number(first.checked) - Number(last.checked));
+      localStorage.setItem('list', JSON.stringify(this.taskList));
+    }
+  }
 }
